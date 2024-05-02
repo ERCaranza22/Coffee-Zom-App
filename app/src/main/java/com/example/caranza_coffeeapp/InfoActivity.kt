b7fef5ac86f4f80a7e9d68ee25e44bab46c983e2
Errol Caranza <errolrensellecaranza@gmail.com>
2024-05-02 22:31:10 +0400

    Create InfoActivity.kt

---
+++ app/src/main/java/com/example/caranza_coffeeapp/InfoActivity.kt
@@ -0,0 +1,11 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.caranza_coffeeapp.R
+
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.popup_info)
+    }
+}
